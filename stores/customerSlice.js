bfb24ccd0e8f8d644715542893b25fd547435534
kipfuho <ctcakip@gmail.com>
2025-03-08 21:40:22 +0700

    feat: customer state

--- stores/customerSlice.js
+++ stores/customerSlice.js
@@ -3,15 +3,31 @@ import { createSlice } from "@reduxjs/toolkit";
 export const customerSlice = createSlice({
   name: "restaurant",
   initialState: {
-    value: {},
+    user: {},
+    cart: {},
+    menu: [],
   },
   reducers: {
-    update: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value = { ...state.value, ...action.payload };
+    // Redux Toolkit allows us to write "mutating" logic in reducers. It
+    // doesn't actually mutate the state because it uses the Immer library,
+    // which detects changes to a "draft state" and produces a brand new
+    // immutable state based off those changes
+    updateMenu: (state, action) => {
+      if (_.get(action, "payload")) return;
+
+      state.menu = action.payload;
+    },
+
+    updateCart: (state, action) => {
+      if (_.get(action, "payload")) return;
+
+      state.cart = action.payload;
+    },
+
+    updateUser: (state, action) => {
+      if (_.get(action, "payload")) return;
+
+      state.user = action.payload;
     },
   },
 });
